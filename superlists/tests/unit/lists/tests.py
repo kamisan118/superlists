eb8a1f47c05d1c2c9b594834012d2a569936f6a9
pmli <kamisan118@gmail.com>
2021-04-25 07:00:29 +0800

    added NewItemTest // pdf-132

--- superlists/tests/unit/lists/tests.py
+++ superlists/tests/unit/lists/tests.py
@@ -118,6 +118,33 @@ class NewListTest(TestCase):
         self.assertEqual(response['location'], '/lists/%d/' % (list_.id,))
 
 
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            'lists/%d/add_item' % (correct_list.id,),
+            data={'item_text', 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            'lists/%d/add_item' % (correct_list.id,),
+            data={'item_text', 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, 'list/%d/' % (correct_list.id,))
+
+
 # 利用 DjangoTestClient, 同時測試 view, model, url mapping (比個別測試要來的更方便..., 是Djago當中特有的寫法)
 class ListViewTest(TestCase):
 
